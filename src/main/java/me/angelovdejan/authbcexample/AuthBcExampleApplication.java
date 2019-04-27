f4cfa8e108daeeb79a018d44e94bada8e77bf1e2
Dejan Angelov <angelovdejan92@gmail.com>
2019-04-27 11:02:19 +0200

    init

---
+++ src/main/java/me/angelovdejan/authbcexample/AuthBcExampleApplication.java
@@ -0,0 +1,13 @@
+package me.angelovdejan.authbcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBcExampleApplication.class, args);
+	}
+
+}
